c86b439f5808c4f2a771933805d3548fc11251f3
Django-B <django.ppp@gmail.com>
2024-05-30 15:29:03 +0300

    first version

---
+++ lua/plugins/init.lua
@@ -0,0 +1,54 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+  	"williamboman/mason.nvim",
+  	opts = {
+  		ensure_installed = {
+  			"lua-language-server", "stylua",
+  			"html-lsp", "css-lsp" , "prettier",
+        "pyright",
+  		},
+  	},
+    keys = {
+      {
+        "<leader>fj",
+        function ()
+          require('hop').hint_words()
+        end,
+        mode = { 'n', 'x', 'o' }
+      }
+    }
+  },
+  {
+    'smoka7/hop.nvim',
+    version = "*",
+    opts = {
+      multi_windows = true,
+      keys = 'etovxqpdygfblzhckisuran',
+      uppercase = true,
+    }
+  },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
